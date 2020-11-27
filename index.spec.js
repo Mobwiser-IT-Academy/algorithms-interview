6de52c287b8e94b4cc4a7659091f4bdd0295101e
Fábio Rito <fabioaarito@MBP-de-Fabio.lan>
2020-11-27 09:34:41 +0000

    fix: fix interview validator

--- index.spec.js
+++ index.spec.js
@@ -1,21 +1,21 @@
-import "./utils/util";
-import "./questions/question-1";
-import "./questions/question-2";
-import "./questions/question-3";
-import "./questions/question-4";
-import "./questions/question-5";
+import './utils/util';
+import './questions/question-1';
+import './questions/question-2';
+import './questions/question-3';
+import './questions/question-4';
+import './questions/question-5';
 
-import "./solutions/question-1";
-import "./solutions/question-2";
-import "./solutions/question-3";
-import "./solutions/question-4";
-import "./solutions/question-5";
-import "./data";
+import './solutions/question-1';
+import './solutions/question-2';
+import './solutions/question-3';
+import './solutions/question-4';
+import './solutions/question-5';
+import './data';
 
 window.questions.forEach((question, index) => {
   test(`Question #${index + 1} - ${question.title}`, () => {
     const exercice = question.exercice;
     const solution = question.solution(exercice.inputs);
-    expect(exercice.output).toBe(solution);
+    expect(exercice.output).toEqual(solution);
   });
 });
